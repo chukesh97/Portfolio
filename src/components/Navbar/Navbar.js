799c5de3917bd6f37b304fb28c700b2433fb7903
chukesh001 <chukesh97@gmail.com>
2023-11-05 01:27:18 +0530

    afsd

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,7 @@
 import "./Navbar.css";
 import { useRef, useEffect, useState } from "react";
 import Logo from "./logo.png";
-import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/dist/js/bootstrap.min.js';
 
 export default function Nav() {
 
