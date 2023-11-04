75976d1d79c7b6c67dcbe6fe19b780231950cc6e
chukesh001 <chukesh97@gmail.com>
2023-11-04 18:15:57 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
-import Layout from './Layout/Layout';
+import "./App.css";
+import Layout from "./Layout/Layout";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
-     <Layout/>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
     </div>
   );
 }
